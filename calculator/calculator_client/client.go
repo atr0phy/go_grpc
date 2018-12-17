28aae2a523dd73994b3c803be25d5b6c7c2c3757
atrophyworks <atrophyworks@gmail.com>
2018-12-18 00:13:49 +0900

    add calculator server streaming

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/atrophyworks/go_grpc/calculator/calculatorpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -18,7 +19,9 @@ func main(){
 
 	c := calculatorpb.NewCalculatorServiceClient(conn)
 
-	doUnary(c)
+	//doUnary(c)
+
+	doServerStreaming(c)
 }
 
 func doUnary(c calculatorpb.CalculatorServiceClient) {
@@ -33,4 +36,25 @@ func doUnary(c calculatorpb.CalculatorServiceClient) {
 		log.Fatalf("error while calling Sum RPC: %v", err)
 	}
 	log.Printf("Response from Sum: %v", res.SumResult)
+}
+
+func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a PrimeDecomposition Server Streaming RPC ...")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 12390392840,
+	}
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling PrimeDecomposition RPC: %v", err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Something happend: %v", err)
+		}
+		fmt.Println(res.GetPrimeFactor())
+	}
 }
