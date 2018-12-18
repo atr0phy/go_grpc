e50dab09978eaef7508fe1d849f5af0904c566d2
atrophyworks <atrophyworks@gmail.com>
2018-12-18 13:41:53 +0900

    add greeting client streaming

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/atrophyworks/go_grpc/greet/greetpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
 	"strconv"
@@ -35,6 +36,26 @@ func (*server) GreetManyTimes (req *greetpb.GreetManyTimesRequest, stream greetp
 		time.Sleep(1000 * time.Millisecond)
 	}
 	return nil
+}
+
+func (*server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
+	fmt.Printf("LongGreet function was invoked with a streaming request\n")
+	result := ""
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// we have finished reading the client stream
+			return stream.SendAndClose(&greetpb.LongGreetResponse{
+				Result: result,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+		firstName := req.GetGreeting().GetFirstName()
+		result += "Hello " + firstName + "! "
+	}
 }
 
 func main(){
